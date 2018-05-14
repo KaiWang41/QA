81267022774a4236439d4c3d1964d4357c5a5404
JulianKWang <164071754@qq.com>
2018-05-14 18:45:34 +1000

    Testing 1st version

--- QA_System.py
+++ QA_System.py
@@ -1,8 +1,6 @@
 import nltk
 import json
 import spacy
-from nltk.corpus import words
-from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics.pairwise import cosine_distances
@@ -12,13 +10,13 @@ OPEN_QUESTION_WORDS = ['what','who','whose','whom','where','when','why','how',
 CLOSED_QUESTION_WORDS = ['is','are','am','was','were','do','does,','did','can',
                          'could','will','would','shall','should','have','has',
                          'had']
-QUESTION_TYPES = ['PERSON','GPE','DATE','MONEY','CARDINAL','ORG','TIME',
-                  'PERCENT','QUANTITY','ORDINAL']
+SIMILARITY_WEIGHT = 0.5
+CORRECT_TYPE_WEIGHT = 0.5
 
 stop = set(stopwords.words('english'))
 
-with open('training.json') as json_data:
-    train = json.load(json_data)
+with open('testing.json') as json_data:
+    test = json.load(json_data)
 
 with open('documents.json') as json_data:
     documents = json.load(json_data)
@@ -39,22 +37,24 @@ def get_BOW_lower_nostop_alpha(sent):
 def get_qword(question):
     tokens = nltk.word_tokenize(question)
     for token in tokens:
-        if token in OPEN_QUESTION_WORDS:
-            return token
+        if token.lower() in OPEN_QUESTION_WORDS:
+            return token.lower()
     for token in tokens:
-        if token in CLOSED_QUESTION_WORDS:
-            return token
+        if token.lower() in CLOSED_QUESTION_WORDS:
+            return token.lower()
     return 'others'
 
 
-for train_case in train:
-    question = train_case['question']
-    docid = train_case['docid']
-    para_num = train_case['answer_paragraph']
+case_count = 0
+for test_case in test:
+    question = test_case['question']
+    docid = test_case['docid']
 
-    para = documents[docid]['text'][para_num]
+    doc = ''
+    for para in documents[docid]['text']:
+        doc += para + ' '
 
-    sents = nltk.sent_tokenize(para)
+    sents = nltk.sent_tokenize(doc)
 
     vectorizer = DictVectorizer()
     BOWs = []
@@ -65,42 +65,325 @@ for train_case in train:
 
     vector1 = vectorizer.transform(get_BOW_lower_nostop_alpha(question))
 
-    sims = []
+    rankings = []
     for sent in sents:
         vector2 = vectorizer.transform(get_BOW_lower_nostop_alpha(sent))
         sim = 1 - cosine_distances(vector1, vector2)
 
-        sims.append(sim)
+        rankings.append(sim * SIMILARITY_WEIGHT)
 
     qword = get_qword(question)
     qtype = 'misc'
-    if qword in ['who',"who's",'whose','whom']:
+    dep = ''
+    head = ''
+    head_dep = ''
+    subject = ''
+    closed_q_choices = ('', '')
+    doc = nlp(question)
+
+    tokens = nltk.word_tokenize(question.lower())
+    if tokens.index(qword) < len(tokens) - 1:
+        next_token = tokens[tokens.index(qword) + 1]
+    else:
+        next_token = ''
+
+    if qword in ['who',"who's",'whom']:
+        qtype = 'noun'
+        for chunk in doc.noun_chunks:
+            if qword in chunk.text:
+                dep = chunk.root.dep_
+                head = chunk.root.head.text
+                head_dep = chunk.root.head.dep_
+
+    elif qword == 'whose':
         qtype = 'PERSON'
+
     elif qword == 'when':
-        qtype = 'DATE'
+        qtype = 'when'
+
     elif qword in ['where',"where's"]:
-        qtype = 'GPE'
+        qtype = 'where'
+
     elif qword in ['how',"how's"]:
-        tokens = nltk.word_tokenize(question)
-        next_token = tokens[tokens.index('qword') + 1]
         if next_token == 'much':
             qtype = 'MONEY'
         elif next_token == 'many':
             qtype = 'CARDINAL'
         elif next_token == 'long':
             qtype = 'DATE'
-        elif next_token == 'far':
+        elif next_token in ['far','big','wide','deep','tall','high']:
             qtype = 'QUANTITY'
-        elif next_token == 'old':
+        elif next_token in ['old','young']:
             qtype = 'DATE'
         else:
-            doc = nlp(question)
+            qtype = 'how'
+            for token in doc:
+                if token.text == qword:
+                    head = token.head.text
+                    head_dep = token.head.dep_
+                    break
 
-    # elif qword in ['what']:
+    elif qword in ['what', "what's", 'which']:
+        if next_token in ['place','year','day','month','age','decade','century']:
+            qtype = 'DATE'
+        elif next_token == 'time':
+            qtype = 'TIME'
+        elif next_token in ['city','country','state']:
+            qtype = 'GPE'
+        elif next_token in ['place','river','mountain','ocean','sea','lake','continent','location']:
+            qtype = 'LOC'
+        elif next_token == 'percentage':
+            qtype = 'PERCENT'
+        elif next_token == 'value':
+            qtype = 'QUANTITY'
+        elif next_token == 'number':
+            qtype = 'CARDINAL'
+        elif next_token == 'price':
+            qtype = 'MONEY'
+        else:
+            tag = nltk.pos_tag([next_token])[0][1]
+            if tag in ['NN','NNS','NNP','NNPS']:
+                qtype = 'noun'
+                for chunk in doc.noun_chunks:
+                    if qword in chunk.text:
+                        dep = chunk.root.dep_
+                        head = chunk.root.head.text
+                        head_dep = chunk.root.head.dep_
+                        break
+            else:
+                tokens.remove(next_token)
+                if 'do' in tokens:
+                    qtype = 'verb'
+                    for token in doc:
+                        if token.dep_ == 'nsubj':
+                            subject = token.text
+                            break
+                else:
+                    qtype = 'noun'
+                    for chunk in doc.noun_chunks:
+                        if qword in chunk.text:
+                            dep = chunk.root.dep_
+                            head = chunk.root.head.text
+                            head_dep = chunk.root.head.dep_
+                            break
 
+    elif qword == 'why':
+        qtype = 'why'
 
-    break
+    elif qword in CLOSED_QUESTION_WORDS:
+        qtype = 'closed'
+        index = tokens.find('or')
+        if index == -1:
+            qtype = 'others'
+            continue
+        prev1 = tokens[index-1]
+        next1 = tokens[index+1]
+        tag_tokens = nltk.pos_tag(tokens)
 
-doc = nlp('When was he born?')
-for token in doc:
-    print(token.subtree)
+        tag = tag_tokens[index-1][1]
+        if tag in ['NN','NNP','NNS','NNPS']:
+            for chunk in doc.noun_chunks:
+                if prev1 in chunk.text:
+                    first = chunk.text
+                if next1 in chunk.text:
+                    second = chunk.text
+            closed_q_choices = (first, second)
+        else:
+            closed_q_choices = (prev1, next1)
+
+    answers = []
+    max_ranking = -1
+    max_idx = -1
+    for (idx, sent) in enumerate(sents):
+        doc = nlp(sent)
+        has_possible_answer = 0
+        answer = ''
+
+        if qtype in ['GPE','DATE','TIME','PERCENT','QUANTITY','CARDINAL',
+                     'MONEY','PERSON','ORG']:
+            for ent in doc.ents:
+                if ent.label_ == qtype:
+                    has_possible_answer = 1
+                    answer = ent.text
+                    break
+
+        elif qtype == 'when':
+            for ent in doc.ents:
+                if ent.label_ == 'TIME' or ent.label_ == 'DATE':
+                    has_possible_answer = 1
+                    answer = ent.text
+                    break
+
+        elif qtype == 'where':
+            for ent in doc.ents:
+                if ent.label_ == 'GPE' or ent.label_ == 'LOC':
+                    has_possible_answer = 1
+                    answer = ent.text
+                    break
+
+        elif qtype == 'how':
+            for token in doc:
+                if token.head.text == head and token.head.dep_ == head_dep:
+                    has_possible_answer = 1
+                    answer = token.text
+                    break
+
+        elif qtype == 'noun':
+            for chunk in doc.noun_chunks:
+                if chunk.root.dep_ == dep and chunk.root.head.text == head and chunk.root.head.dep_ == head_dep:
+                    has_possible_answer = 1
+                    answer = chunk.text
+                    break
+
+        elif qtype == 'verb':
+            for token in doc:
+                if token.dep_ == 'nsubj' and token.text == subject:
+                    has_possible_answer = 1
+                    for token1 in doc:
+                        if token1.dep_ == 'ROOT':
+                            answer = token1.text
+                            break
+                    break
+
+        elif qtype == 'closed':
+            first = closed_q_choices[0]
+            second = closed_q_choices[1]
+            appear1 = False
+            appear2 = False
+            negate1 = False
+            negate2 = False
+            neg_count = 0
+            tokens = nltk.word_tokenize(sent)
+
+            for (index, token) in enumerate(tokens):
+                if token == 'not' or "n't" in token:
+                    neg_count += 1
+
+                    if index+1 < len(tokens):
+                        if tokens[index+1] == first:
+                            negate1 = True
+                        if tokens[index+1] == second:
+                            negate2 = True
+
+                if token == first:
+                    appear1 = True
+                if token == second:
+                    appear2 = True
+
+            if (not appear1) and (not appear2):
+                answer = second
+
+            elif appear1 and not appear2:
+                has_possible_answer = 1
+                if neg_count % 2 == 1:
+                    answer = second
+                else:
+                    answer = first
+
+            elif appear2 and not appear1:
+                has_possible_answer = 1
+                if neg_count % 2 == 0:
+                    answer = second
+                else:
+                    answer = first
+
+            else:
+                has_possible_answer = 1
+                if negate1 and not negate2:
+                    answer = second
+                elif negate2 and not negate1:
+                    answer = first
+                else:
+                    answer = second
+
+        elif qtype == 'why':
+            if 'reason' in sent or 'because' in sent or 'due to' in sent or 'since' in sent or 'for' in sent:
+                has_possible_answer = 1
+
+                if 'because of' in sent:
+                    index = sent.index('because of')
+                    substr = sent[index+11:]
+                    span = nlp(substr)
+                    for chunk in span.noun_chunks:
+                        answer = chunk.text
+                        break
+
+                elif 'because' in sent:
+                    index = sent.index('because of')
+                    substr = sent[index + 11:]
+                    answer = substr[:-1]
+
+                elif 'due to' in sent:
+                    index = sent.index('due to')
+                    substr = sent[index+7:]
+                    span = nlp(substr)
+                    for chunk in span.noun_chunks:
+                        answer = chunk.text
+                        break
+                    if answer == '':
+                        answer = substr[:-1]
+
+                elif 'reason' in sent:
+                    index = sent.index('reason')
+                    substr = sent[index+7:]
+                    span = nlp(substr)
+                    for chunk in span.noun_chunks:
+                        answer = chunk.text
+                        break
+                    if answer == '':
+                        index = substr.find('is')
+                        if index != -1:
+                            answer = substr[index+3:-1]
+                        else:
+                            index = substr.find('are')
+                            if index != -1:
+                                answer = substr[index+4:-1]
+                            else:
+                                answer = sent[sent.index('reason'):-1]
+
+                elif 'for' in sent:
+                    index = sent.index('for')
+                    substr = sent[index + 4:]
+                    span = nlp(substr)
+                    for chunk in span.noun_chunks:
+                        answer = chunk.text
+                        break
+                    if answer == '':
+                        answer = substr[:-1]
+
+                elif 'since' in sent:
+                    index = sent.index('since')
+                    substr = sent[index + 6:]
+                    answer = substr[:-1]
+
+        else:
+            for chunk in doc.noun_chunks:
+                if chunk.text not in question:
+                    has_possible_answer = 1
+                    answer = chunk.text
+                    break
+
+        rankings[idx] += has_possible_answer * CORRECT_TYPE_WEIGHT
+        answers.append(answer)
+
+        if rankings[idx] > max_ranking:
+            max_ranking = rankings[idx]
+            max_idx = idx
+
+    answer = ''
+    if answers[max_idx] != '':
+        answer = answers[max_idx]
+    else:
+        sent = sents[max_idx]
+        doc = nlp(sent)
+        for chunk in doc.noun_chunks:
+            if chunk.text not in question:
+                answer = chunk.text
+                break
+        if answer == '':
+            for chunk in doc.noun_chunks:
+                answer = chunk.text
+                break
+
+    print(case_count, ',', answer, sep='')
+    case_count += 1
